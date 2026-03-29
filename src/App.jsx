af3117d748cdf711756ab2380471c47538113b26
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-03-29 14:20:11 +0100

    routed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
       <h1>Bsf_nutrifeed</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-data" element={<AddData />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
